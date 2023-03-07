1537f053e5735699ef35d55c20890d52361ae21c
javawithlwl <javawithlwl@gmail.com>
2023-03-07 07:17:40 +0530

    First commit of march batch

---
+++ src/main/java/com/careerit/cj/day1/SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.careerit.cj.day1;
+
+public class SumOfTwoNumbers {
+  public static void main(String[] args) {
+      int a = 10;
+      int b = 20;
+      int sum = a + b;
+    System.out.println(a+" + "+b+" = "+sum);
+  }
+
+}
